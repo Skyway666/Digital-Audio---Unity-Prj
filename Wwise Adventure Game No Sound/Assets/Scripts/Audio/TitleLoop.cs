038306c5c8726f13fd037f7a3515061f948edd66
Skyway666 <lucasgarciamateu@gmail.com>
2019-06-08 12:02:33 +0200

    Quest 1.1.2 and 1.1.3 completed
    Watashi wa Hero janai... WATASHI WA KAMI DA!

---
+++ Wwise Adventure Game No Sound/Assets/Scripts/Audio/TitleLoop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleLoop : MonoBehaviour
+{
+    AudioSource source;
+
+
+    float clip_started = 0;
+    float clip_duration = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        clip_duration = 82;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - clip_started >= clip_duration)
+        {
+            clip_started = Time.time;
+            source.time = 6;
+            source.Play();
+            clip_duration = 76;
+        }
+    }
+}
